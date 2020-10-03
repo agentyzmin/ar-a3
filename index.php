4309540e09e9ef5ea2494de5e0b2f079b964c836
Aleksandr Kolodko Mac16 <alexkolodko@gmail.com>
2020-10-03 12:32:15 +0300

    fix mobile view

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
   	<title>A3 AR</title>
   	<link rel="stylesheet" type="text/css"
   	      href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">	
 	
   	<meta name="description"
   	      content="Міські об’єкти у доповненій реальності.">
